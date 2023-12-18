e2eb7d8ea38341145ed56bacc40d6cccec98c6eb
AlexShin <alex.shin@yale.edu>
2023-12-18 03:20:45 -0500

    final version

--- mctsold.py
+++ mctsold.py
@@ -10,50 +10,44 @@ class Node:
         self.children = []
         self.wins = 0
         self.visits = 1
-        self.actions = state.get_actions()
-        self.leaf = True
-        self.state_dict = state_dict
+        self.untried_actions = set(state.get_actions())
         self.isterminal = state.is_terminal()
         self.actor = state.actor()
         self.offset = 1 if self.actor == 0 else -1
+        self.state_dict = state_dict
 
     def add_child(self, action, state):
         child_node = Node(state, self.state_dict, parent=self, action=action)
         self.children.append(child_node)
+        self.untried_actions.remove(action)
         return child_node
-    
+
     def best_child(self, c_param=math.sqrt(2)):
         choices_weights = [
-            self.offset * (child.wins/child.visits) + c_param * math.sqrt((2*math.log(self.visits)/child.visits))
+            self.offset * (child.wins/child.visits) + c_param * math.sqrt((2 * math.log(self.visits) / child.visits))
             for child in self.children
         ]
-        return self.children[choices_weights.index((max(choices_weights)))]
-    
+        return self.children[choices_weights.index(max(choices_weights))]
+
     def rollout_policy(self, possible_moves):
         return random.choice(possible_moves)
-    
+
     def update(self, result):
         self.visits += 1
         self.wins += result
 
-def select_node(node, visited_nodes):
+def select_node(node):
     while not node.isterminal:
-        if node.leaf:
-            temp = expand_node(node)
-            visited_nodes.append(node)
-            return temp
+        if node.untried_actions:
+            return expand_node(node)
         else:
             node = node.best_child()
-        visited_nodes.append(node)
     return node
 
 def expand_node(node):
-    for action in node.actions:
-        next_state = node.state.successor(action)
-        node.add_child(action, next_state)
-        
-    node.leaf = False
-    return random.choice(node.children)
+    action = random.choice(list(node.untried_actions))
+    next_state = node.state.successor(action)
+    return node.add_child(action, next_state)
 
 def simulate_random_game(node):
     current_state = node.state
@@ -63,39 +57,28 @@ def simulate_random_game(node):
         current_state = current_state.successor(action)
     return current_state.payoff()
 
-def backpropagate(result, visited_nodes):
-    for node in visited_nodes:
+def backpropagate(node, result):
+    while node is not None:
         node.update(result)
+        node = node.parent
 
-def mcts(state_dict, root_state, time_limit):
-    if(root_state in state_dict):
+def mcts(root_state, time_limit, state_dict):
+    if root_state in state_dict:
         root_node = state_dict[root_state]
-    else: 
+    else:
         root_node = Node(root_state, state_dict)
         state_dict[root_state] = root_node
     end_time = time.time() + time_limit
 
     while time.time() < end_time:
-        visited_nodes = []
-        selected_node = select_node(root_node, visited_nodes)
+        selected_node = select_node(root_node)
         result = simulate_random_game(selected_node)
-        backpropagate(result, visited_nodes)
-    
-    maxPayoff = -1 * float("inf")
-    maxAction = None
+        backpropagate(selected_node, result)
 
-    for i in range(len(root_node.children)):
-        child = root_node.children[i]
-        payoff = root_node.offset * (child.wins/child.visits)
-        if payoff > maxPayoff:
-            maxPayoff = payoff
-            maxAction = child.action
-
-    return maxAction
+    return root_node.best_child(c_param=0).action
 
 def mcts_policy(time_limit):
     state_dict = dict()
-
     def policy(state):
-        return mcts(state_dict, state, time_limit)
+        return mcts(state, time_limit, state_dict)
     return policy
